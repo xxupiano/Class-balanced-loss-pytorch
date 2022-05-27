0c5a6cf5ba7c9fb0239df67e1a1cd537c67e9590
Xin Xu <xuxin1999102@126.com>
2022-05-27 10:12:22 +0800

    Update class_balanced_loss.py

--- class_balanced_loss.py
+++ class_balanced_loss.py
@@ -48,6 +48,21 @@ class FocalLoss(nn.Module):
 
 
 class CBLoss(nn.Module):
+     """Compute the Class Balanced Loss between `logits` and the ground truth `labels`.
+    Class Balanced Loss: ((1-beta)/(1-beta^n))*Loss(labels, logits)
+    where Loss is one of the standard losses used for Neural Networks.
+    Args:
+      labels: A int tensor of size [batch].
+      logits: A float tensor of size [batch, no_of_classes].
+      samples_per_cls: A python list of size [no_of_classes], number per class.
+      no_of_classes: total number of classes. int
+      loss_type: string. One of "sigmoid", "focal", "softmax".
+      beta: float. Hyperparameter for Class balanced loss.
+      gamma: float. Hyperparameter for Focal loss.
+    Returns:
+      cb_loss: A float tensor representing class balanced loss
+    """
+        
     def __init__(self,datasetpath,loss_type="focal", beta=0.9999, gamma=2.0):
         super(CBLoss,self).__init__()
         self.loss_type = loss_type
